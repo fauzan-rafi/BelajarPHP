965563838e2091b4c25ec5899f88fc5c5a440904
fauzan-rafi <fauzanrafi002@gmail.com>
2021-01-27 20:53:59 +0700

    hari ke-3

--- pertemuan2/condition.php
+++ pertemuan2/condition.php
@@ -1,13 +1,55 @@
 <?php
 
-$x = 70;
-
-if ( $x == 10) {
-      echo "kondisi tersebut benar";
-}else {
-      echo "kondisi tersebut salah";
+//penggunaan else if
+$hari = "jumat";
+echo "<br>";
+if ($hari == "senin") {
+      echo "Hari senin";
+} elseif ($hari == "selasa") {
+      echo "Hari selasa";
+} elseif ($hari == "rabu") {
+      echo "Hari rabu";
+} elseif ($hari == "kamis") {
+      echo "Hari Kamis";
+} else {
+      // echo " sekarang bukan hari senin,selasa,rabu,dan kamis";
 }
-
-
-
 ?>
+<br>
+<?php
+// $nilai = 70;
+// if ($nilai <= 20) { //rentang 0-20
+//       echo "Siswa dapat predikat E";
+// } elseif ($nilai <= 40) { //rentang 21 - 40
+//       echo "Siswa dapat predikat D";
+// } elseif ($nilai <= 60) { //rentang 41 - 60
+//       echo "Siswa dapat predikat C";
+// } elseif ($nilai <= 80) { // rentang 61 - 80
+//       echo "Siswa dapat predikat B";
+// } else {
+//       echo "Siswa dapat predikat A";
+// }
+?>
+<?php
+// switch
+$nilai = 70;
+switch ($nilai) {
+      case $nilai <= 20:
+            echo "Siswa dapat predikat E";
+            break;
+      case $nilai <= 40:
+            echo "Siswa dapat predikat D";
+            break;
+      case $nilai <= 60:
+            echo "Siswa dapat predikat C";
+            break;
+      case $nilai <= 80:
+            echo "Siswa dapat predikat B";
+            break;
+      default:
+            echo "Siswa dapat predikat A";
+            break;
+}
+// selesai mengeksekusi switch
+echo "<br>";
+echo "test";
---
+++ pertemuan3/looping.php
@@ -0,0 +1,24 @@
+<?php 
+//do ... while
+$y = 10;
+do {
+      echo "test <br>";
+      $y--;
+} while ($y > 0);
+
+//menggunakan while
+$x = 10;
+
+while ($x  == 0) {
+      echo "variable x lebih besar dari 0 <br>";
+      $x--;
+}
+
+// menggunakan for loop
+//mencetak angka 1 - 10
+for ($i = 1; $i < 10; $i++) { 
+      echo "$i";
+      echo "<br>";
+}
+echo "test";
+?>
